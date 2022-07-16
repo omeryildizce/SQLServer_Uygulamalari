12001696cb1cddeddef4536965ad64d3d8c822e5
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:09:04 +0300

    Create c08_top_and_percent.sql

---
+++ bolum_03_Gruplandirma_ve_Operatorler/c08_top_and_percent.sql
@@ -0,0 +1,13 @@
+-- top
+-------------------
+
+
+select top 10 * from Ogrenciler 
+
+select top 5 * from Ogrenciler where OGRCINSIYET = 'erkek'
+
+
+-- top
+-------------------
+select top 40 percent * from Ogrenciler
+
