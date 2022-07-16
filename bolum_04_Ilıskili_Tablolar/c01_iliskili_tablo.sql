02a590c7217e9d7f140e2b1d34e1132f398a6f6b
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:09:12 +0300

    Create c01_iliskili_tablo.sql

---
+++ bolum_04_Ilıskili_Tablolar/c01_iliskili_tablo.sql
@@ -0,0 +1,17 @@
+-- Ýliþkili tablolar
+---- veri karmaþasýný önler
+---- veri tekrarýný önler
+---- bellek ferformansýný arttýrýr
+
+-- Türler
+---- Bire bir
+---- Bire Çok
+---- Çoka çok
+
+
+update Ogrenciler set OGRKULUP = 1 where OGRKULUP='Biliþim'
+update Ogrenciler set OGRKULUP = 2 where OGRKULUP='Kitaplýk'
+update Ogrenciler set OGRKULUP = 3 where OGRKULUP='Satranç'
+update Ogrenciler set OGRKULUP = 4 where OGRKULUP='Gezi'
+update Ogrenciler set OGRKULUP = 5 where OGRKULUP='Temizlik' 
+
