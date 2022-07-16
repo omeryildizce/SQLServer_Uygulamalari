4a0843eb7a75ac27a66338692127c0e9a2bdde36
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:08:32 +0300

    Create c03_Insert.sql

---
+++ bolum_02_DDL_ve_DML/c03_Insert.sql
@@ -0,0 +1,13 @@
+ -- Insert
+------------------------------
+
+insert into Ogrenciler (OGRAD, OGRSOYAD, OGRCINSIYET, OGRKULUP, OGRSEHIR)
+values ('Bayram', 'Yüce', 'Erkek', 'Gezi', 'Erzurum')
+select * from Ogrenciler
+
+insert into Notlar([OGRENCI], [DERS], [SINAV1], [SINAV2], [SINAV3], [ORTALAMA], [DURUM])
+values (6, 2, 80, 60, 70, 70, 1)
+select * from Notlar
+
+
+
