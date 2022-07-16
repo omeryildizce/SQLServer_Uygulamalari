e7adca1a143cea7fb45048dcdb528cbd6148a4cf
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:08:30 +0300

    Create c03_Delete.sql

---
+++ bolum_02_DDL_ve_DML/c03_Delete.sql
@@ -0,0 +1,9 @@
+-- Delete
+--------------------------
+
+-- tablodaki tüm deðerleri silmek için kullanýlan komut
+-- delete from Dersler
+
+delete from Dersler where DERSID = 4
+
+select * from Dersler
