f10ce3a8c5b8e8563509a8311c11dc3d410b1376
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:09:33 +0300

    Create c01_string_islemleri.sql

---
+++ bolum_07_String_islemleri/c01_string_islemleri.sql
@@ -0,0 +1,17 @@
+select upper('merhaba')
+select lower('mERhaBA')
+
+
+select SUBSTRING(ograd, 1,3) from Ogrenciler
+select left('merhaba dünya', 4 )
+select right('merhaba dünya', 4 )
+select len('merhaba dünya')
+
+select LTRIM('            Merhaba           Dünya          ')
+select RTRIM('            Merhaba           Dünya          ')
+
+select REPLACE('Merhaba Dünya', 'Dünya', 'MSSQL')
+
+select CHARINDEX('dü', 'merhaba dünya' )
+
+select REverse('Merhaba Dünya')
