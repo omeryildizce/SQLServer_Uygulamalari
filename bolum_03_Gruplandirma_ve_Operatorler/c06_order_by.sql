f40661ea70345e8747b9f713c973c94c081753e5
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:08:57 +0300

    Create c06_order_by.sql

---
+++ bolum_03_Gruplandirma_ve_Operatorler/c06_order_by.sql
@@ -0,0 +1,14 @@
+-- order by
+-----------------------
+
+select * from Ogrenciler 
+order by OGRAD
+
+select * from Ogrenciler 
+order by OGRAD asc
+
+select * from Ogrenciler 
+order by OGRAD desc
+
+select * from Ogrenciler 
+order by OGRCINSIYET asc
