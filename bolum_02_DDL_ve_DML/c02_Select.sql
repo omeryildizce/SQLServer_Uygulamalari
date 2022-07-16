9b2c88daa350976fed9f15b6dbeba8298b89a0ab
omeryildizce <omeryildizce@gmail.com>
2022-07-16 19:08:27 +0300

    Create c02_Select.sql

---
+++ bolum_02_DDL_ve_DML/c02_Select.sql
@@ -0,0 +1,24 @@
+-- DML Komutlarý
+-- Select
+-- Insert
+-- Update
+-- Delete
+
+
+-- select
+------------------------------
+
+select * from Ogrenciler
+
+select OGRAD,OGRSOYAD from Ogrenciler
+
+select OGRSEHIR from Ogrenciler
+
+select * from Ogrenciler where OGRAD = 'Ali'
+
+select * from Ogrenciler where OGRSEHIR = 'Ankara' and OGRKULUP = 'Santraç'
+
+select * from Ogrenciler where OGRSEHIR = 'Ankara' or OGRKULUP = 'Santraç'
+
+
+
