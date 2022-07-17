6449c295518cf5e5c5d8b9fbf7a1da3ab3979de1
omeryildizce <omeryildizce@gmail.com>
2022-07-17 11:26:37 +0300

    Create c02_date_name.sql

---
+++ bolum_10_Tarih/c02_date_name.sql
@@ -0,0 +1,10 @@
+select datename(MONTH, GETDATE()) 
+
+select datename(MONTH, GETDATE()) , DATENAME(MONTH, '1999.12.12')
+
+
+select DATENAME(DAY, getdate())
+
+select DATENAME(WEEKDAY, getdate())
+
+select DATENAME(WEEKDAY, '2001.11.11')
