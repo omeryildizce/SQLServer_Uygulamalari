dfba62926f0d19cabb1ca43e3c1288f6ee3feb0a
omeryildizce <omeryildizce@gmail.com>
2022-07-17 11:26:55 +0300

    Create c01_View.sql

---
+++ bolum_13_View/c01_View.sql
@@ -0,0 +1,16 @@
+create view test1 
+as 
+select * from Kategori
+
+select * from Kategori
+
+select * from test1
+
+alter view test1
+as 
+select * from Kategori
+where KategoriId < 4
+
+select * from test2
+
+select *  from test3
