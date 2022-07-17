9f050306af6ebaa90505afa6af60ce6363c80e3a
omeryildizce <omeryildizce@gmail.com>
2022-07-17 12:48:04 +0300

    Create c02_commit.sql

---
+++ bolum_18_Transaction/c02_commit.sql
@@ -0,0 +1,26 @@
+use Yeni
+begin transaction 
+insert into Urun (Ad,Stok) values ('Ürün3',25)
+insert into Urun (Ad,Stok) values ('Ürün4','45a')
+
+select * from Urun
+commit
+
+
+
+select * from Urun
+
+use Yeni
+begin transaction 
+insert into Urun (Ad,Stok) values ('Ürün1',25)
+insert into Urun (Ad,Stok) values ('Ürün2',40)
+
+select * from Urun
+commit
+
+
+
+select * from Urun
+
+
+
